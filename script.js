d3df71e34fc1fccf9ac0257ce697ca0be415abf2
Izaelle Lavigne <izaelleavrillavigne@gmail.com>
2023-10-09 17:08:48 -0300

    firts commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeScene(page) {
+  const screens = document.querySelectorAll(".screen")
+  const selectedScreen = document.querySelector("."+page)
+  for(const screen of screens) {
+    if(!screen.classList.contains('hide')){
+      screen.classList.add('hide')
+    }
+  }
+  
+  selectedScreen.classList.remove('hide')
+}
+
+// const screen1 = document.querySelector(".screen-home")
+// if (screen1.classList.contains("hide")) {
+//   screen1.classList.remove("hide")
+// } else {
+//   screen1.classList.add("hide")
+// }
